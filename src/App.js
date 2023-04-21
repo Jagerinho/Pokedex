c772ecd481830cbdb46f43db9c1ec427d777d77a
Danny <ceapadansebastian@gmail.com>
2023-04-21 09:30:52 +0200

    added home and about routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import Main from './Components/Main';
 import './Components/style.css'
-import { Link, Outlet } from "react-router-dom"
+import About from "./routes/AboutMe.js";
+import Home from "./Components/Main.jsx";
+import { Route, HashRouter, Link, Routes  } from "react-router-dom";
 
-export default function App() {
+
+// export default function App() {
+//     return (
+//         <>
+//             <Main/>
+//            <Outlet/>
+//         </>
+//     )
+
+function Navigation() {
     return (
-        <>
-            {/* <nav>
-                <Link to="/">Home</Link>
-    </nav>*/}
-            <Main/>
-           <Outlet/>
-        </>
+      <nav>
+            <Link className="links" to="/">Home</Link>
+            <Link className="links" to="/about">About</Link>
+      </nav>
+    );
+  }
+
+const App = () => {
+    return (
+        <HashRouter>
+        <div>
+            <Navigation />
+            <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            </Routes>
+        </div>
+      </HashRouter>
     )
 }
+
+export default App;
---
+++ src/routes/AboutMe.js
@@ -0,0 +1,9 @@
+import "./AboutMe.css"
+
+export default function Aboutme(){
+    return <>
+    <div className="background">
+    <h1>Me are you, and you are me</h1>
+    </div>
+    </>
+}
